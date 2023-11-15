0c4b66f3951df9776116001983cf56b393a4a923
emelieskepo <skepo@student.chalmers.se>
2023-11-15 14:47:55 +0100

    15 november pass 2

--- src/CarTransporter.java
+++ src/CarTransporter.java
@@ -1,40 +1,83 @@
 import java.awt.*;
+import java.util.ArrayList;
 
-public class CarTransporter extends Truck{
+    public class CarTransporter extends Truck {
 
-    public CarTransporter(int nrDoors, double enginePower, Color color, String modelName, boolean isLiftUp) {
-        super(nrDoors, enginePower, color, modelName, isLiftUp);
-    }
+        private ArrayList<Car> loadedCars;
 
-    public void lowerRamp() {
-        if (!isMoving()) {
-            setIsLiftUp(true);
+        public CarTransporter(int nrDoors, double enginePower, Color color, String modelName, boolean isLiftUp) {
+            super(nrDoors, enginePower, color, modelName, isLiftUp);
+            this.loadedCars = new ArrayList<>();
         }
+
+        public void lowerRamp() { //sänk rampen mot marken så att bil kan köra på
+            if (!isMoving()) {
+                setIsLiftUp(false);
+            } else {
+                setIsLiftUp(true); // om vi kör så måste rampen vara uppe
+            }
+        }
+
+        private boolean isCarClose() { // är bilen i närheten
+            // vi vill hitta något car x eller y
+            //if car close enough - true
+            // else false
+        }
+
+
+        public void getXPosCar() {
+            // return Car.getXPos();
+
+        }
+
+        //om bilen är tillräckligt nära CarTransporter och rampen är ner så läggs den till i listan
+        public void loadCar() {
+            if (!getIsLiftUp() && isCarClose()) {//lista med bilar
+
+                //double carX = getXPos() + 1;
+                // när bilen är på transportern ska den ha samma position som transportern
+            }
+        }
+
+        public void unloadCar() {
+            if (!getIsLiftUp() &&) {//lista med bilar
+                // hamnar rimligt nära rampen när den är avlastad
+
+            }
+        }
+
+        //public double getYPos() {
+        //        return yPos;
+        //    }
+        //
+        //    public double getXPos() {
+        //        return xPos;
+        //    }
+
+        @Override
+        public double speedFactor() {
+            return 0;
+        }
+
     }
 
-    @Override
-    public double speedFactor() {
-        return 0;
-    }
 
 
+        //Biltransportens ramp har endast två lägen, uppe eller nere. - true/false KLAR
 
-    //Biltransportens ramp har endast två lägen, uppe eller nere. - true/false
+        //Rampen kan endast vara nere om biltransporten står stilla. KLAR
 
-    //Rampen kan endast vara nere om biltransporten står stilla.
+        //Bilar kan endast lastas om rampen är nere, och de befinner
+        //sig rimligt nära biltransporten (gör ett eget antagande,
+        //de exakta detaljerna är inte viktiga).
 
-    //Bilar kan endast lastas om rampen är nere, och de befinner
-    //sig rimligt nära biltransporten (gör ett eget antagande,
-    //de exakta detaljerna är inte viktiga).
+        //Bilar kan endast lossas om rampen är nere. De bör då hamna rimligt nära biltransporten.
 
-    //Bilar kan endast lossas om rampen är nere. De bör då hamna rimligt nära biltransporten.
+        //Bilar kan endast lossas i omvänd ordning från hur de lastades,
+        //dvs den sista bilen som lastades måste vara först att lossas (first-in-last-out).
 
-    //Bilar kan endast lossas i omvänd ordning från hur de lastades,
-    //dvs den sista bilen som lastades måste vara först att lossas (first-in-last-out).
+        //Biltransporten ska inte kunna lasta på en annan biltransport.
 
-    //Biltransporten ska inte kunna lasta på en annan biltransport.
+        //Under det att en bil är lastad på biltransporten ska dess position i
+        //världen alltid vara densamma som biltransportens position.
 
-    //Under det att en bil är lastad på biltransporten ska dess position i
-    //världen alltid vara densamma som biltransportens position.
-
-}
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,78 +2,80 @@ import java.awt.*;
 
 public abstract class Vehicle implements Movable{
 
-    private int nrDoors; // Number of doors on the car
-    private double enginePower; // Engine power of the car
-    private double currentSpeed; // The current speed of the car
-    private Color color; // Color of the car
-    private String modelName; // The car model name
-    private double xPos = 0;
-    private double yPos = 0;
-    private String direction = "NORTH";
+        private Position position;
+        private int nrDoors; // Number of doors on the car
+        private double enginePower; // Engine power of the car
+        private double currentSpeed; // The current speed of the car
+        private Color color; // Color of the car
+        private String modelName; // The car model name
 
-    public Vehicle(int nrDoors, double enginePower, Color color, String modelName){
-        this.nrDoors = nrDoors;
-        this.enginePower = enginePower;
-        this.color = color;
-        this.modelName = modelName;
-        stopEngine();
-    }
-
-    public int getNrDoors() {
-        return nrDoors;
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public double getYPos() {
-        return yPos;
-    }
-
-    public double getXPos() {
-        return xPos;
-    }
-
-    public double getEnginePower() {
-        return enginePower;
-    }
-
-    public double getCurrentSpeed() {
-        return currentSpeed;
-    }
-
-    public void setCurrentSpeed(double currentSpeed) {
-        if(currentSpeed >= 0 && currentSpeed <= this.enginePower){
-            this.currentSpeed = currentSpeed;
+        public Vehicle(int nrDoors, double enginePower, Color color, String modelName){
+            this.nrDoors = nrDoors;
+            this.enginePower = enginePower;
+            this.color = color;
+            this.modelName = modelName;
+            this.position = new Position(0, 0, "NORTH");
+            stopEngine();
         }
-    }
 
-    public Color getColor() {
-        return color;
-    }
+        public int getNrDoors() {
+            return nrDoors;
+        }
 
-    public void setColor(Color clr) {
-        color = clr;
-    }
+        public String getDirection() {
+            return position.getDirection();
+            //return direction;
+        }
 
-    public void startEngine() {
-        currentSpeed = 0.1;
-    }
+        public double getYPos() {
+            return position.getYPos();
+            //return yPos;
+        }
 
-    public void stopEngine() {
-        currentSpeed = 0;
-    }
+        public double getXPos() {
+            return position.getXPos();
+            //return xPos;
+        }
 
-    public abstract double speedFactor(); //metoden finns men den är inte implementerad
+        public double getEnginePower() {
+            return enginePower;
+        }
 
-    public void incrementSpeed(double amount){
-        setCurrentSpeed(getCurrentSpeed() + speedFactor() * amount);
-    }
+        public double getCurrentSpeed() {
+            return currentSpeed;
+        }
 
-    public void decrementSpeed(double amount){
-        setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
-    }
+        public void setCurrentSpeed(double currentSpeed) {
+            if(currentSpeed >= 0 && currentSpeed <= this.enginePower){
+                this.currentSpeed = currentSpeed;
+            }
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public void setColor(Color clr) {
+            color = clr;
+        }
+
+        public void startEngine() {
+            currentSpeed = 0.1;
+        }
+
+        public void stopEngine() {
+            currentSpeed = 0;
+        }
+
+        public abstract double speedFactor(); //metoden finns men den är inte implementerad
+
+        public void incrementSpeed(double amount){
+            setCurrentSpeed(getCurrentSpeed() + speedFactor() * amount);
+        }
+
+        public void decrementSpeed(double amount){
+            setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
+        }
 
     public void move() {
         switch (direction) {
@@ -85,35 +87,35 @@ public abstract class Vehicle implements Movable{
     }
 
     public void turnLeft() {
-        switch (direction) {
-            case "NORTH" -> this.direction = "WEST";
-            case "EAST" -> this.direction = "NORTH";
-            case "SOUTH" -> this.direction = "EAST";
-            case "WEST" -> this.direction = "SOUTH";
+        switch (position.getDirection()) {
+            case "NORTH" -> position.setDirection("WEST");
+            case "EAST" -> position.setDirection("NORTH");
+            case "SOUTH" -> position.setDirection("EAST");
+            case "WEST" -> position.setDirection("SOUTH");
         }
     }
 
     public void turnRight() {
-        switch (direction) {
-            case "WEST" -> this.direction = "NORTH";
-            case "NORTH" -> this.direction = "EAST";
-            case "EAST" -> this.direction = "SOUTH";
-            case "SOUTH" -> this.direction = "WEST";
+        switch (position.getDirection()) {
+            case "WEST" -> position.setDirection("NORTH");
+            case "NORTH" -> position.setDirection("EAST");
+            case "EAST" -> position.setDirection("SOUTH");
+            case "SOUTH" -> position.setDirection("WEST");
         }
     }
 
-    public void gas(double amount) {
-        if (amount >= 0 && amount <= 1) {
-            incrementSpeed(amount);
+        public void gas(double amount) {
+            if (amount >= 0 && amount <= 1) {
+                incrementSpeed(amount);
+            }
+
         }
 
-    }
-
-    public void brake(double amount) {
-        if (amount >= 0 && amount <= 1) {
-            decrementSpeed(amount);
+        public void brake(double amount) {
+            if (amount >= 0 && amount <= 1) {
+                decrementSpeed(amount);
+            }
         }
+
+
     }
-
-
-}
